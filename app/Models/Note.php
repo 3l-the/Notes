2707495b4b30587d9bb9c42eddea4a1c5bf6ea5c
Eduardo Mulhia <emulhiad@gmail.com>
2024-04-25 22:30:30 -0600

    Creación de notas con su categoria

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -11,7 +11,7 @@ class Note extends Model
     use HasFactory;
 
     protected $fillable = [
-        'title', 'content', 'active'
+        'title', 'content', 'active', 'category_id'
     ];
 
     // protected $guarded = [];
