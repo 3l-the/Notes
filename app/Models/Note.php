ae705ffa26c8adf6558318261fc137b4fe180697
Eduardo Mulhia <emulhiad@gmail.com>
2024-04-25 22:31:16 -0600

    Correcciones

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -21,11 +21,6 @@ class Note extends Model
     }
 
     static function nota_por_id($id){
-        $notes = DB::table('notes')
-        ->join('categories', 'categories.id', '=', 'notes.category_id')
-        ->select('notes.id','notes.title', 'notes.content', 'categories.category_name')
-        ->where('notes.active', true)
-        ->get();
         return Note::where('id', $id)
             ->where('active', true)
             ->firstOrFail();
